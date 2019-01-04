6ae1cf9355ed37224f32dbadf4baa311fad9699b
Your Name <you@example.com>
2019-01-04 14:25:52 -0500

    Please work

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
         'USER': 'chang_kim',
         'PASSWORD': '0908chang',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
         }
 }
 
