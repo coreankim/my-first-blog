50b2f1e2c0f78abedd7c6f81f73ef8e6118eb4de
Your Name <you@example.com>
2019-01-08 16:48:18 -0500

    pushed

--- blog/views.py
+++ blog/views.py
@@ -27,23 +27,22 @@ def sidebar(request):
 
 def plan_detail(request, Injury_key):
 	plan = get_object_or_404(Plan, Injury_key=Injury_key)
-	acute_plan = plan.Acute_plan.split("\n")
-	print(acute_plan)
+	acute_plan_split = plan.Acute_plan.split("\n")
 	newTextList = list()
-	for i in range(len(acute_plan)):
+	for i in range(len(acute_plan_split)):
 		newText = ""
-		if "##" in acute_plan[i]:
-			newText = "<h5><u>"+acute_plan[i].replace("##","")+"</u></h5>"
-		elif "~~" in acute_plan[i]:
-			newText = "<ul><li>"+acute_plan[i].replace("~~","").lstrip()+"</li></ul>"
-		elif acute_plan[i] == "":
+		if "##" in acute_plan_split[i]:
+			newText = "<h5><u>"+acute_plan_split[i].replace("##","")+"</u></h5>"
+		elif "~~" in acute_plan_split[i]:
+			newText = "<ul><li>"+acute_plan_split[i].replace("~~","").lstrip()+"</li></ul>"
+		elif acute_plan_split[i] == "":
 			newText = "<br>"
 		else:
-			newText = "<li>"+acute_plan[i]+"</li>"
+			newText = "<li>"+acute_plan_split[i]+"</li>"
 		newTextList.append(newText)
 	acute_plan_edited = " ".join(newTextList)
 	if plan.Category in ["Procedures", "Rotations_logistics", "Attending Preferences", "Templates", "Contact Info", "Education Sources"]:
-		plan.Acute_plan = plan.Acute_plan
+		plan.Acute_plan = acute_plan_edited
 	else:
 		plan.Acute_plan = acute_plan_edited
 	return render(request, 'blog/plan_detail.html', {'plan': plan})
